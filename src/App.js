a7f0acbc3b796875fde0c472aa638988e2c9d657
catfishona <1972034@maranatha.ac.id>
2022-09-30 01:08:44 +0800

    feat: change paths to blue on click

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React, { useState } from "react";
 import "./App.css";
 import Flower from "./components/Flower";
 
 function App() {
+  const [fillColor, setFillColor] = useState(Array(12).fill("white"));
+  const [currentColor, setCurrentColor] = useState("blue");
+
+  const onFillColor = (i) => {
+    let newFillColors = fillColor.slice(0)
+    newFillColors[i] = currentColor
+    setFillColor(newFillColors)
+  }
+
   return (
     <div className="App">
-      <Flower />
+      <Flower fillColor={fillColor} onFill={onFillColor}/>
     </div>
   );
 }
